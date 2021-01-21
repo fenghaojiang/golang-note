1363486775ee64b50919121fdd0b3548fcd3ccc5
fenghaojiang <457420285@qq.com>
2021-01-21 18:29:18 +0800

    Update moveFilesTools.go

--- moveFilesTools.go
+++ moveFilesTools.go
@@ -1,6 +1,17 @@
 package main
 
+import (
+	"io/ioutil"
+	"strings"
+)
+
 func main() {
 	suffix := ".md"
-	
+	fileDir := "./"
+	files, _ := ioutil.ReadDir(fileDir)
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), suffix) {
+
+		}
+	}
 }
